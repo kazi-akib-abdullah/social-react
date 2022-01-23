ac29f53e5c1a1fc78d27c2f4cae3dd8f1a214ec4
kazi-akib-abdullah <q.aqib.abdullah@gmail.com>
2022-01-24 04:03:51 +0600

    updated

--- src/SignInScreen.jsx
+++ src/SignInScreen.jsx
@@ -1,7 +1,11 @@
 import { authentication } from './firebase-config';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
+
 const SignInScreen = ()=>{
+    const logout = () => {
+      authentication.signOut();
+    }
     const signInWithGoogle = ()=>{
         const provider = new GoogleAuthProvider();
         signInWithPopup(authentication, provider)
@@ -11,13 +15,21 @@ const SignInScreen = ()=>{
         .catch((err)=>{
           console.log(err)
         })
+        authentication.onAuthStateChanged( async (user)=>{
+          if(user){
+            let token = await user.getIdToken();
+            console.log(token);
+          }
+        })
       }
     return (
         <div className="App">
             <button onClick={signInWithGoogle}>
               Click me
             </button>
+            <button onClick={logout}>Logout</button>
         </div>
+        
       );
 }
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    // add your firebase config
+    apiKey: "AIzaSyDGYzG2MxISstNAQcCVn3x4qrnbKQ_r0K4",
+    authDomain: "fir-ae42b.firebaseapp.com",
+    projectId: "fir-ae42b",
+    storageBucket: "fir-ae42b.appspot.com",
+    messagingSenderId: "521077883479",
+    appId: "1:521077883479:web:dd93347f06b6f69b4ed912",
+    measurementId: "G-X05SEB8S96"
 };
 
 const app = initializeApp(firebaseConfig);
